bf7fea05a6d3669ceb6cb8af90041b1d4a09a866
Ricardo <49110228+Deodutt@users.noreply.github.com>
2021-11-27 22:41:57 -0500

    basic flask application w/ Dockerfile and k8s yaml

---
+++ application/flask/application.py
@@ -0,0 +1,60 @@
+from __future__ import unicode_literals
+from flask import Flask, request, render_template, send_file
+from werkzeug.utils import secure_filename
+import helper
+import logging
+from pytube import YouTube
+
+application = app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def converter():
+    # Needs to be global for the /download route
+    global youtube_url, video_details
+    user_url = ""
+    youtube_url = ""
+    video_details = {}
+    helper.delete()
+
+    if request.method == "POST" and "youtube_link" in request.form:
+        user_url = str(request.form.get("youtube_link")).strip()
+
+        video_details = helper.webscrapper(user_url)
+        youtube_url = video_details.get("video_url")
+
+    return render_template(
+        "index.html",
+        video_details=video_details
+    )
+
+
+@app.route("/download", methods=["GET", "POST"])
+def download():
+    try:
+        try:
+            download_path = (
+                YouTube(youtube_url)
+                .streams.get_audio_only()
+                .download(filename=f"{video_details.get('video_title')}.mp3")
+            )
+        except:
+            download_path = (
+                YouTube(youtube_url)
+                .streams.get_audio_only()
+                .download(filename=f"{video_details.get('video_id')}.mp3")
+            )
+
+        fname = download_path.split("//")[-1]
+
+        return send_file(
+            fname,
+            as_attachment=True,
+        )
+
+    except:
+        logging.exception("Failed download")
+        return "Video download failed!"
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0')
---
+++ application/flask/helper.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+import requests
+import os 
+from os import listdir
+
+
+def webscrapper(user_url):
+    video_details = {}
+
+    req = requests.get(user_url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+
+    video_details['video_title'] = soup.find(
+        "meta", itemprop="name").attrs.get('content')
+
+    video_details['video_author'] = soup.find(
+        "link", itemprop="name").attrs.get('content')
+
+    video_details['video_views'] = "{:,}".format(int(soup.find(
+        "meta", itemprop="interactionCount").attrs.get('content')))
+
+    video_details['video_published'] = soup.find(
+        "meta", itemprop="datePublished").attrs.get('content')
+
+    video_details['video_duration'] = soup.find(
+        "meta", itemprop="duration").attrs.get('content')
+
+    video_details['video_id'] = soup.find(
+        "meta", itemprop="videoId").attrs.get('content')
+
+    video_details['video_thumbnail'] = soup.find(
+        "link", itemprop="thumbnailUrl").attrs.get('href')
+
+    video_details[
+        'video_url'] = f"https://www.youtube.com/watch?v={video_details.get('video_id')}"
+
+    # print(video_details)
+
+    return video_details
+
+def delete():
+    folder_path = './'
+    for file_name in listdir(folder_path):
+        if file_name.endswith('.mp3'):
+            os.remove(folder_path + file_name)
